38dd20c80f08cf41bc0ed7e4e13d747aa96172b1
Sang Tran <dragonageorigins123@gmail.com>
2024-12-06 16:24:22 +0700

    feat: add ccip doc

---
+++ docs/apps/ccip-bridge/overview.md
@@ -0,0 +1,13 @@
+---
+description: A dApp for bridging ERC-20 tokens and NFTs (non-fungible tokens) (coming soon) between the Ronin chain and others chain.
+slug: /apps/ccip-bridge
+title: CCIP Bridge
+---
+
+## Overview
+
+CCIP Bridge ([app.roninchain.com/ccip-bridge](https://app.roninchain.com/ccip-bridge)) is an app for bridging ERC-20 tokens and NFTs (non-fungible tokens) (coming soon) between the Ronin chain and others chain.
+
+## User guides
+
+* [Transfer an ERC-20 token](./guides/transfer-token.md)
--- sidebars.js
+++ sidebars.js
@@ -91,7 +91,7 @@ const sidebars = {
           label: 'Ronin Name Service',
           link: {
             type: 'doc',
-            id: 'apps/rns/overview', 
+            id: 'apps/rns/overview',
           },
           items: [
             {
@@ -125,7 +125,7 @@ const sidebars = {
           label: 'Ronin Bridge',
           link: {
             type: 'doc',
-            id: 'apps/ronin-bridge/overview', 
+            id: 'apps/ronin-bridge/overview',
           },
           items: [
             // Deposit ERC-20 tokens
@@ -138,7 +138,20 @@ const sidebars = {
             'apps/ronin-bridge/guides/withdraw-nft',
             // Token withdrawal limits
             'apps/ronin-bridge/reference/withdrawal-limits',
-          ]
+          ],
+        },
+        // CCIP Bridge
+        {
+          type: 'category',
+          label: 'CCIP Bridge',
+          link: {
+            type: 'doc',
+            id: 'apps/ccip-bridge/overview',
+          },
+          items: [
+            // Transfer ERC-20 tokens
+            'apps/ccip-bridge/guides/transfer-token',
+          ],
         },
         // Katana
         {
@@ -146,7 +159,7 @@ const sidebars = {
           label: 'Katana',
           link: {
             type: 'doc',
-            id: 'apps/katana/overview', 
+            id: 'apps/katana/overview',
           },
           items: [
             // Swap tokens
@@ -158,7 +171,7 @@ const sidebars = {
               link: {
                 type: 'generated-index',
                 description: 'Guides for creating and managing liquidity pools.',
-                slug: 'apps/katana/pools'
+                slug: 'apps/katana/pools',
               },
               items: [
                 // Create a liquidity pool
@@ -205,12 +218,9 @@ const sidebars = {
       link: {
         type: 'generated-index',
         slug: '/rpc',
-        description: 'Guides for setting up and using an RPC node.',        
+        description: 'Guides for setting up and using an RPC node.',
       },
-      items: [
-        'rpc/mainnet-rpc',
-        'rpc/testnet-rpc',
-      ],
+      items: ['rpc/mainnet-rpc', 'rpc/testnet-rpc'],
     },
     // Delegators
     {
@@ -218,7 +228,7 @@ const sidebars = {
       label: 'Delegators',
       link: {
         type: 'doc',
-        id: 'delegators/index',        
+        id: 'delegators/index',
       },
       items: [
         // Stake RON
@@ -239,7 +249,7 @@ const sidebars = {
       label: 'Validators',
       link: {
         type: 'doc',
-        id: 'validators/index',        
+        id: 'validators/index',
       },
       items: [
         {
@@ -247,7 +257,7 @@ const sidebars = {
           label: 'Setup',
           link: {
             type: 'doc',
-            id: 'validators/setup/overview',   
+            id: 'validators/setup/overview',
           },
           items: [
             {
@@ -256,7 +266,7 @@ const sidebars = {
               link: {
                 type: 'generated-index',
                 slug: '/validators/setup/mainnet',
-                description: 'Node installation guides for the Ronin mainnet.'
+                description: 'Node installation guides for the Ronin mainnet.',
               },
               items: [
                 // Run a validator and bridge together
@@ -321,8 +331,9 @@ const sidebars = {
           label: 'Manage validator account',
           link: {
             type: 'generated-index',
-            title: 'Manage your validator account', 
-            description: 'Guides for increasing or withdrawing your stake, claiming rewards, and managing your validator account.',
+            title: 'Manage your validator account',
+            description:
+              'Guides for increasing or withdrawing your stake, claiming rewards, and managing your validator account.',
             slug: '/validators/manage',
           },
           items: [
@@ -362,7 +373,7 @@ const sidebars = {
           label: 'Governance',
           link: {
             type: 'doc',
-            id: 'validators/governance/overview',        
+            id: 'validators/governance/overview',
           },
           items: [
             // Create and vote on proposals
@@ -378,7 +389,7 @@ const sidebars = {
       label: 'Bridge operators',
       link: {
         type: 'doc',
-        id: 'bridge-operators/index',   
+        id: 'bridge-operators/index',
       },
       items: [
         {
@@ -386,12 +397,9 @@ const sidebars = {
           label: 'Setup',
           link: {
             type: 'doc',
-            id: 'bridge-operators/setup/overview',   
+            id: 'bridge-operators/setup/overview',
           },
-          items: [
-            'bridge-operators/setup/run-bridge',
-            'bridge-operators/setup/upgrade-bridge',
-          ],
+          items: ['bridge-operators/setup/run-bridge', 'bridge-operators/setup/upgrade-bridge'],
         },
         // Slashing
         'bridge-operators/slashing',
@@ -401,7 +409,7 @@ const sidebars = {
           label: 'Governance',
           link: {
             type: 'doc',
-            id: 'bridge-operators/governance/overview',        
+            id: 'bridge-operators/governance/overview',
           },
           items: [
             // Create and vote on proposals
